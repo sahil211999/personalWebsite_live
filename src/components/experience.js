e2e21066e5316c900cd1991fb192555828058c55
sahil_21 <ssashi2@illinois.edu>
2020-10-29 02:49:36 -0500

    files

--- src/components/experience.js
+++ src/components/experience.js
@@ -43,7 +43,7 @@ class Experience extends Component{
          <div > 
          <Container style={{padding:'50px'}} >
          <Row style = {{paddingBottom:'50px', fontWeight:'bolder'}}><Col><h1 style = {{fontSize:'50px'}}>Cargill Innovation Lab</h1></Col></Row>
-         <Row><Col lg={true}> <Image src={require('/Users/sahil_21/Desktop/new_website/new_website2/new_website2/src/Assets/cargill-1.png')}  rounded style ={{height:'500px', width:'500px', border:'1.0px solid black' }} /> </Col> <Col lg={true} style={{ color:'white', fontFamily:'Montserrat', fontSize:'20px', fontWeight:'bolder' }}><p>I worked as a full stack developer collaborating with UX designers, buisness analysts and actual users from focus groups to develop products which could one day be a part of the array of digital services Cargill provides. </p> 
+         <Row><Col lg={true}> <Image src={require('../Assets/cargill-1.png')}  rounded style ={{height:'500px', width:'500px', border:'1.0px solid black' }} /> </Col> <Col lg={true} style={{ color:'white', fontFamily:'Montserrat', fontSize:'20px', fontWeight:'bolder' }}><p>I worked as a full stack developer collaborating with UX designers, buisness analysts and actual users from focus groups to develop products which could one day be a part of the array of digital services Cargill provides. </p> 
          <p> For my core project, I developed a responsive web app for animal feed mills to improve user experience. Apart from my core team project, I contributed to another hardware project at the Cargill Innovation, developing the UI for a hardware project, which was aimed at improving order accuracy in quick service restaurants. This prototype will potetially be pitched to YUM brands. </p>
          <p> I also developed a prototype feedmill truck driver app for tracking order and optmizing routes</p>
          </Col> </Row>
@@ -57,7 +57,7 @@ class Experience extends Component{
          <Container style={{padding:'50px'}} >
          <Row style = {{ paddingBottom:'50px' ,fontWeight:'bolder'}}><Col><h1 style = {{fontSize:'50px'}}>QA infotech</h1></Col></Row>
          <Row><Col lg={true} style={{ fontFamily:'Montserrat', fontSize:'20px', fontWeight:'bolder' }}> <p> I worked as a backend developer on a full-stack development team. I worked on the Java SpringBoot framework, developing APIs for a farm risk management app. I contributed in designed and architecting the backend database for the APIs and also helped in deployment of the the application. I also worked on quality assurance testing and debugging code for streamlining the application. </p> </Col> <Col   >
-         <Image src={require('/Users/sahil_21/Desktop/new_website/new_website2/new_website2/src/Assets/QaInfotech.jpeg')}  rounded style ={{height:'500px', width:'500px', border:'6px solid black' }} />
+         <Image src={require('../Assets/QaInfotech.jpeg')}  rounded style ={{height:'500px', width:'500px', border:'6px solid black' }} />
          </Col> </Row>
          </Container>
           </div>
@@ -67,7 +67,7 @@ class Experience extends Component{
          <div> 
          <Container style={{padding:'50px'}} >
          <Row style = {{paddingBottom:'50px', fontWeight:'bolder'}}><Col lg={true}><h1 style = {{fontSize:'50px'}}>NLP Research at UIUC</h1></Col></Row>
-         <Row><Col lg={true}> <Image src={require('/Users/sahil_21/Desktop/new_website/new_website2/new_website2/src/Assets/1_tYZ8_SodwSUPJTuWQs-CoA.jpeg')}  rounded style ={{height:'500px', width:'500px', border:'6px solid black' }} /> </Col> <Col  style={{ color:'white', fontFamily:'Montserrat', fontSize:'20px', fontWeight:'bolder' }}><p> Working on emotion analysis to detect emotions from fiction novels particularly from project Gutenberg. I am currently working on collecting, catalouging and labelling data from fiction novels and </p> 
+         <Row><Col lg={true}> <Image src={require('../Assets/1_tYZ8_SodwSUPJTuWQs-CoA.jpeg')}  rounded style ={{height:'500px', width:'500px', border:'6px solid black' }} /> </Col> <Col  style={{ color:'white', fontFamily:'Montserrat', fontSize:'20px', fontWeight:'bolder' }}><p> Working on emotion analysis to detect emotions from fiction novels particularly from project Gutenberg. I am currently working on collecting, catalouging and labelling data from fiction novels and </p> 
          <p> </p>
         </Col> </Row>
          </Container>
